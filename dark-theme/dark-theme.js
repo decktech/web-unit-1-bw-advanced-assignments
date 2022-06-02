bbacd9a57c323d58ed4c9e236cd65ca153dd3643
Matt Decker <matt@Matts-MBP.lan>
2022-06-02 17:56:40 -0400

    dark-theme complete

--- dark-theme/dark-theme.js
+++ dark-theme/dark-theme.js
@@ -1,12 +1,26 @@
 // you need to create DOM selectors to grab the correct HTML elements and update their styles to the opposite theme's css classes when you want to toggle the theme off and on
-let btn = document.querySelector("button");
-let box = document.querySelector("div");
 
-btn.addEventListener('click', function(){
-    if(box.className == ""){
-        box.className == "dark-bg"
-    } else{
-        box.className = "";
-    }
+const checkbox = document.getElementById('checkbox');
+let box = document.querySelector('div');
+
+checkbox.addEventListener('change', function() {
+    box.classList.toggle('dark')
 })
+
+console.log(checkbox);
+
+
+
+
+
+// let btn = document.querySelector("button");
+// let box = document.querySelector("div");
+
+// btn.addEventListener('click', function(){
+//     if(box.className == ""){
+//         box.className == "dark-bg"
+//     } else{
+//         box.className = "";
+//     }
+// })
 // console.log(box)
